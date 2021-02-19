ac137776ddc7e7c8746a76798795162a03374931
Lazy-fox <lazy-fox@yandex.ru>
2021-02-19 21:49:04 +0300

    BT serviceUuid = null

--- amplify-frontend/src/components/ConnectDevice.vue
+++ amplify-frontend/src/components/ConnectDevice.vue
@@ -32,7 +32,8 @@ export default {
   methods: {
     connect: function () {
       let self = this;
-      let serviceUuid = '49535343-fe7d-4ae5-8fa9-9fafd205e455'
+      let serviceUuid = null; 
+      //let serviceUuid = '49535343-fe7d-4ae5-8fa9-9fafd205e455'
       let options = {
         optionalServices: [serviceUuid],
         // filters: [{
